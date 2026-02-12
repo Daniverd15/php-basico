85f210e912c41560ebb82337f7c28b24b9781ded
Daniel Enrique Villamizar <dvillamizar435@unab.edu.co>
2026-02-12 10:54:35 -0500

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/mi-nombre', function () {
+    return view('Daniel Enrique Villamizar Ramírez');
+});
